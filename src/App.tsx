d418fae1e049099907470c513f603112ae4accdd
g33kism <gunrajsm@gmail.com>
2024-02-14 13:34:57 -0500

    personalize

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,16 @@ export default function Page() {
       "What if I asked really nicely?",
       "Pretty please",
       "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
+      "What about a frosticino",
+      "PLEASE ZOOZOO",
       "But :*(",
       "I am going to die",
       "Yep im dead",
-      "ok ur talking to nathan's ghost",
+      "ok ur talking to gunraj's ghost",
       "please babe",
       ":((((",
       "PRETTY PLEASE",
-      "Estoy muerto",
+      "Shundi?",
       "No :(",
     ];
 
@@ -46,7 +46,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Daisy, will you be my Valentine?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
